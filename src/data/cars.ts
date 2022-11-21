470c7da7f25d7c6cb639b5622b354dc30fce88e8
AndyGaSa <garcia.andy.tecnologies10@gmail.com>
2022-11-21 14:35:16 +0100

    Create alias for src path

---
+++ src/data/cars.ts
@@ -0,0 +1,155 @@
+export const People = [
+    {
+      id: '1',
+      name: 'Ibiza',
+      category: 'car',
+      'category-image': 'car.png',
+      company: 'Seat',
+      'company-image': 'seat.png',
+      CV: '30'
+    },
+    {
+      id: '2',
+      name: 'Arona',
+      category: 'car',
+      'category-image': 'car.png',
+      company: 'Seat',
+      'company-image': 'seat.png',
+      CV: '100'
+    },
+    {
+      id: '3',
+      name: 'Leon',
+      category: 'car',
+      'category-image': 'car.png',
+      company: 'Seat',
+      'company-image': 'seat.png',
+      CV: '50'
+    },
+    {
+      id: '4',
+      name: 'Leon Sportstourer',
+      category: 'car',
+      'category-image': 'car.png',
+      company: 'Seat',
+      'company-image': 'seat.png',
+      CV: '100'
+    },
+    {
+      id: '5',
+      name: 'Alejandro Pedros',
+      category: 'car',
+      'category-image': 'car.png',
+      company: 'Seat',
+      'company-image': 'seat.png',
+      CV: '10'
+    },
+    {
+      id: '6',
+      name: 'Ateca',
+      category: 'car',
+      'category-image': 'car.png',
+      company: 'Seat',
+      'company-image': 'seat.png',
+      CV: '97'
+    },
+    {
+      id: '7',
+      name: 'Tarraco',
+      category: 'car',
+      'category-image': 'car.png',
+      company: 'Seat',
+      'company-image': 'seat.png',
+      CV: '21'
+    },
+    {
+      id: '8',
+      name: 'MO 125',
+      category: 'motorcycle',
+      'category-image': 'motorcycle.png',
+      company: 'Seat',
+      'company-image': 'seat.png',
+      CV: '10'
+    },
+    {
+      id: '9',
+      name: 'New MO 125',
+      category: 'motorcycle',
+      'category-image': 'motorcycle.png',
+      company: 'Seat',
+      'company-image': 'seat.png',
+      CV: '10'
+    },
+    {
+      id: '10',
+      name: 'MO 65',
+      category: 'scooter',
+      'category-image': 'scooter.png',
+      company: 'Seat',
+      'company-image': 'seat.png',
+      CV: '55'
+    },
+    {
+      id: '11',
+      name: 'Formentor',
+      category: 'car',
+      'category-image': 'car.png',
+      company: 'Cupra',
+      'company-image': 'Cupra.png',
+      CV: '156'
+    },
+    {
+      id: '12',
+      name: 'Born',
+      category: 'car',
+      'category-image': 'car.png',
+      company: 'Cupra',
+      'company-image': 'Cupra.png',
+      CV: '156'
+    },
+    {
+      id: '13',
+      name: 'Leon',
+      category: 'car',
+      'category-image': 'car.png',
+      company: 'Cupra',
+      'company-image': 'Cupra.png',
+      CV: '156'
+    },
+    {
+      id: '14',
+      name: 'Leon Sportstourer',
+      category: 'car',
+      'category-image': 'car.png',
+      company: 'Cupra',
+      'company-image': 'Cupra.png',
+      CV: '156'
+    },
+    {
+      id: '15',
+      name: 'e-Racer',
+      category: 'car',
+      'category-image': 'car.png',
+      company: 'Cupra',
+      'company-image': 'Cupra.png',
+      CV: '156'
+    },
+    {
+      id: '16',
+      name: 'Leon Competicion',
+      category: 'car',
+      'category-image': 'car.png',
+      company: 'Cupra',
+      'company-image': 'Cupra.png',
+      CV: '156'
+    },
+    {
+      id: '17',
+      name: 'Ateca',
+      category: 'car',
+      'category-image': 'car.png',
+      company: 'Cupra',
+      'company-image': 'Cupra.png',
+      CV: '156'
+    },
+  ];
